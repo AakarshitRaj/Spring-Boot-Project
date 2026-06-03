415a753b37d74e1da95fae21e244ebbc99889ade
Aakarshit Raj <rajaakarshit7@gmail.com>
2026-06-04 01:57:37 +0530

    Employee-Management Basic spring security with admin + user

--- Employee-Management-DTO/src/main/java/com/employee/config/SecurityConfig.java
+++ Employee-Management-DTO/src/main/java/com/employee/config/SecurityConfig.java
@@ -1,13 +1,36 @@
 package com.employee.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+    //own user password
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails admin= User.withDefaultPasswordEncoder()
+								.username("admin")
+								.password("admin123")
+								.roles("ADMIN")
+								.build();
+		
+		UserDetails user=User.withDefaultPasswordEncoder()
+								.username("user")
+								.password("user123")
+								.roles("USER")
+								.build();
+		
+		return new InMemoryUserDetailsManager(admin,user);
+								
+	}
+	
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
